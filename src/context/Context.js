82f9dad5667e4c7bbe2d2a04d2a4c737cd88a592
nunoo <nunoo@nunoo.local>
2020-04-30 15:56:33 -0700

    feat(singleorder): added order method and styling

--- src/context/Context.js
+++ src/context/Context.js
@@ -3,8 +3,8 @@ import { MEALS } from '../data/dummy-data';
 
 // Set my state
 const initialState = {
-  meals: MEALS,
-  orders: [],
+	meals: MEALS,
+	orders: [],
 };
 
 // Initiate Context
@@ -12,54 +12,54 @@ const MealContext = createContext();
 
 // Add Reducer if using reducer
 const mealReducer = (state, action) => {
-  switch (action.type) {
-    case 'add_to_order':
-      return { ...state, orders: state.orders.concat(action.order) };
+	switch (action.type) {
+		case 'add_to_order':
+			return { ...state, orders: state.orders.concat(action.order) };
 
-    case 'remove_order':
-      return {
-        ...state,
-        orders: state.orders.filter(order => order.id !== action.orderId),
-      };
+		case 'remove_order':
+			return {
+				...state,
+				orders: state.orders.filter((order) => order.id !== action.orderId),
+			};
 
-    case 'get_orders':
-      return state;
+		case 'get_orders':
+			return state;
 
-    // create set_favorite case that returns the updated state
+		// create set_favorite case that returns the updated state
 
-    default:
-      return state;
-  }
+		default:
+			return state;
+	}
 };
 
 // Create provider component and export not as default!
 export const MealProvider = ({ children }) => {
-  // in component useState or useReducer for functionality
-  const [state, dispatch] = useReducer(mealReducer, initialState);
+	// in component useState or useReducer for functionality
+	const [state, dispatch] = useReducer(mealReducer, initialState);
 
-  const addToOrder = order => {
-    let createOrderId = Math.random().toString(36).substring(2, 15);
-    order.id = createOrderId;
-    dispatch({ type: 'add_to_order', order });
-  };
+	const addToOrder = (order) => {
+		let createOrderId = Math.random().toString(36).substring(2, 15);
+		order.id = createOrderId;
+		dispatch({ type: 'add_to_order', order });
+	};
 
-  const getOrders = () => {
-    dispatch({ type: 'get_orders' });
-  };
+	const getOrders = () => {
+		dispatch({ type: 'get_orders' });
+	};
 
-  const removeOrder = orderId => {
-    dispatch({ type: 'remove_order', orderId });
-  };
+	const removeOrder = (orderId) => {
+		dispatch({ type: 'remove_order', orderId });
+	};
 
-  // Add a setFavorite method that updates the favorite property in meal model from false to true
-  // mealId should be passed as the parameter and passed through the provider to be accessed
+	// Add a setFavorite method that updates the favorite property in meal model from false to true
+	// mealId should be passed as the parameter and passed through the provider to be accessed
 
-  // return provider
-  return (
-    <MealContext.Provider value={{ state, addToOrder, getOrders, removeOrder }}>
-      {children}
-    </MealContext.Provider>
-  );
+	// return provider
+	return (
+		<MealContext.Provider value={{ state, addToOrder, getOrders, removeOrder }}>
+			{children}
+		</MealContext.Provider>
+	);
 };
 
 export default MealContext;
--- src/screens/SingleMeal.js
+++ src/screens/SingleMeal.js
@@ -1,45 +1,139 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import {
+	Text,
+	View,
+	StyleSheet,
+	FlatList,
+	TouchableOpacity,
+	Button,
+	ImageBackground,
+	Picker,
+	TextInput,
+	TouchableWithoutFeedback,
+} from 'react-native';
 import MealContext from '../context/Context';
 
-const SingleMealScreen = props => {
-  const { state, addToOrder } = useContext(MealContext);
-  const meal = state.meals.find(meal => meal.id === props.route.params.mealId);
-  const [order, setOrder] = useState({ meal: {}, quantity: 1 });
+// const state = {
+// 	quantity: '',
+// };
 
-  const orderMeal = () => {
-    order.meal = meal;
-    addToOrder(order);
-  };
+const SingleMealScreen = (props) => {
+	const { state, addToOrder } = useContext(MealContext);
+	const meal = state.meals.find(
+		(meal) => meal.id === props.route.params.mealId
+	);
+	const [order, setOrder] = useState({ meal: {}, quantity: 1 });
 
-  return (
-    <View style={styles.container}>
-      {/* Meal Information and Description */}
-      <View style={styles.infoCard}>
-        <Text>{meal.title}</Text>
-      </View>
+	const orderMeal = () => {
+		order.meal = meal;
+		addToOrder(order);
+	};
 
-      {/* Order Now button and input for quantity */}
-      <View style={styles.actionButtons}>
-        {/* I need a quantity input :/ */}
-        <Button title='Order Now!' />
-      </View>
-    </View>
-  );
+	// console.log(order);
+
+	return (
+		<View style={styles.mealItem}>
+			<TouchableWithoutFeedback>
+				<View>
+					{/* Top Card Section */}
+					{/* Meal Information and Description */}
+					<View style={{ ...styles.mealRow, ...styles.mealHeader }}>
+						<ImageBackground
+							source={{ uri: meal.imageUrl }}
+							style={styles.imgBG}
+						>
+							<View style={styles.titleContainer}>
+								<Text style={styles.title}>{meal.title}</Text>
+							</View>
+						</ImageBackground>
+					</View>
+
+					{/* Bottom Card Section */}
+					<View style={[styles.mealRow, styles.mealDetail]}>
+						<Text style={{ ...styles.title, ...{ color: 'black' } }}>
+							${meal.price}
+						</Text>
+						<Text>({meal.affordability})</Text>
+
+						<View style={styles.actionButtons}>
+							{/* I need a quantity input :/ */}
+
+							<Button
+								title="Order Now!"
+								onPress={() => {
+									orderMeal(order);
+
+									console.log(order);
+								}}
+							/>
+						</View>
+					</View>
+				</View>
+			</TouchableWithoutFeedback>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    margin: 10,
-  },
-  infoCard: {
-    height: 100,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#999',
-    borderRadius: 5,
-  },
+	actionButtons: {
+		height: 40,
+		backgroundColor: 'white',
+		borderRadius: 20,
+	},
+	textInputStyle: {
+		height: 20,
+		width: 40,
+		marginRight: 20,
+		borderColor: 'gray',
+		borderWidth: 1,
+		backgroundColor: 'white',
+	},
+	mealItem: {
+		height: 500,
+		width: '100%',
+		backgroundColor: '#ddd',
+		marginBottom: 15,
+		borderRadius: 10,
+		overflow: 'hidden',
+	},
+	imgBG: {
+		width: '100%',
+		height: '100%',
+		justifyContent: 'flex-end',
+	},
+	mealRow: {
+		flexDirection: 'row',
+	},
+	mealHeader: {
+		height: '85%',
+	},
+	mealDetail: {
+		paddingHorizontal: 10,
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		height: '15%',
+	},
+	titleContainer: {
+		backgroundColor: 'rgba(0,0,0,0.5)',
+		paddingVertical: 5,
+		paddingHorizontal: 12,
+	},
+	title: {
+		fontSize: 22,
+		color: 'white',
+		textAlign: 'center',
+	},
+	button: {
+		height: 20,
+		width: 40,
+		borderRadius: 10,
+		backgroundColor: 'white',
+	},
+	buttonText: {
+		margin: 2,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
 
 export default SingleMealScreen;
